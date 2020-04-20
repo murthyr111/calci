ce32ea4d3c61c4b71249dc1aa17f5f0adc624006
Rama Murthy <rmurthy@progress.com>
2020-04-20 18:44:48 +0530

    first commit

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,21 @@
+
+class Calculator {
+	
+	public long addition(long a, long b) {
+		System.out.println("addition");
+		return a+b;
+	}
+	
+	public long substraction(long a, long b) {
+		return a-b;
+	}
+	
+	public long multiplication(long a, long b) {
+		return a*b;
+	}
+	
+	public long division(long a, long b) {
+		return a/b;
+	}
+
+}
---
+++ src/test/java/TestCalculator.java
@@ -0,0 +1,25 @@
+
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.Assertions;
+
+public class TestCalculator {
+	
+	Calculator calculator = new Calculator();
+	
+	@Test
+	public void testAddition() {
+		Assertions.assertEquals(13, calculator.addition(10, 3));
+	}
+	
+	@Test
+	public void testSubstraction() {
+		Assertions.assertEquals(2, calculator.substraction(5, 3));
+	}
+	
+	@Test
+	public void testMultiplication() {
+		Assertions.assertEquals(15, calculator.multiplication(5, 3));
+	}
+
+}
